a41c071fe33bab8a349dfa190b9ccd250991f1e3
Parjanya Roy <parjanyaroy@gmail.com>
2022-03-09 11:43:25 +0530

    Roy Prep - Committing Changes for LWC_IDE . Adding functionality to save LWC component bundles

--- force-app/main/default/lwc/lwcEditorWindow/lwcEditorWindow.js
+++ force-app/main/default/lwc/lwcEditorWindow/lwcEditorWindow.js
@@ -1,5 +1,6 @@
 import { LightningElement , wire , track } from 'lwc';
 import getResourceBundleForId from '@salesforce/apex/LWCDetailsHelper.getResourceBundleForId';
+import saveLWCBundles from '@salesforce/apex/LWCDetailsHelper.saveLWCBundles'
 import {
     subscribe,
     APPLICATION_SCOPE,
@@ -9,8 +10,6 @@ import recordSelected from '@salesforce/messageChannel/lwcnavigator__c';
 import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
 import highlightminjs from '@salesforce/resourceUrl/HighlightMinJS';
 
-const NUM_OF_TABS = 30;
-
 export default class LwcEditorWindow extends LightningElement {
     subscription = null;
     selectedComponentId = null;
@@ -19,12 +18,56 @@ export default class LwcEditorWindow extends LightningElement {
     isLoading = false;
     @wire(MessageContext)
     messageContext;
+    lwcBundleChangeList = [];
+    lwcBundleChangeListSize ;
+    
+    saveLWCBundle()
+    {
+        console.log(this.lwcBundleChangeList);
+        saveLWCBundles({updatedResourceBundle : this.lwcBundleChangeList})
+        .then((response)=>{
+            console.log(response);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
+    handleTextChange(event)
+    {
+        var pBundleId = event.currentTarget.dataset.id;
+        var pBundleChangedValue =event.target.value;
+        /*console.log(this.lwcBundleChangeList.some(function(el) {
+            return el.bundleId === pBundleId;}));
+        console.log(this.lwcBundleChangeList.map(function(e) { return e.bundleId; }).indexOf(pBundleId));*/
+        if(this.lwcBundleChangeList.some(function(el) {
+            return el.bundleId === pBundleId;})){ // If condition checks if a javascript object already exists in the Array with the same bundle ID
+                var existingIndex = this.lwcBundleChangeList.map(function(e) { return e.bundleId; }).indexOf(pBundleId); // Get the existing object so that the change ccode can be used to replace the older value
+                if(existingIndex!==-1){
+                    this.lwcBundleChangeList[existingIndex]={
+                        Id : pBundleId ,
+                        Source : pBundleChangedValue 
+                    };
+                }
+                else{
+                    alert('Error');
+                }
+            }
+            else{
+                this.lwcBundleChangeList.push({
+                    Id : pBundleId ,
+                    Source : pBundleChangedValue 
+                });
+            }
+        
+        console.log(this.lwcBundleChangeList);
+    }
 
     @wire(getResourceBundleForId,{ lwcId : '$selectedComponentId'})
     getResourceBundle(response)
-    {   this.isLoading=true;
+    {   
+        this.isLoading=true;
         this.selectedComponentBundleData = response.data;
-        console.log(this.selectedComponentBundleThankData);
+        console.log(this.selectedComponentBundleData);
         this.isLoading=false;
     }
 
@@ -32,16 +75,14 @@ export default class LwcEditorWindow extends LightningElement {
     {
         this.subscribeToMessageChannel();
     }
-    renderedCallback()
+    reloadComponentBundle()
     {
-        loadScript(this, highlightminjs)
-            .then(() => { 
-                console.log('Loaded HighlightMinJS');
-                this.hightlightAll();
-            })
-            .catch(error => console.log(error));
-        
+        console.log('reload bundle ');
+        eval("$A.get('e.force:refreshView').fire();");
     }
+
+    
+
 
     subscribeToMessageChannel() {
         this.subscription = subscribe(
@@ -59,6 +100,7 @@ export default class LwcEditorWindow extends LightningElement {
         this.selectedComponentId = message.lwcId;
         this.selectedComponentName = message.lwcName;
         this.isLoading=false;
+        //this.reloadComponentBundle();
 
     }
 
